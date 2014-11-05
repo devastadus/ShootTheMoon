cc4804a3383a4e32623297a101c68a583fb1e2bc
devastadus <scottprice7@gmail.com>
2014-11-04 21:53:25 -0800

    update

---
+++ Assets/Scripts/Character.js
@@ -0,0 +1,52 @@
+﻿#pragma strict
+var lastX:float;
+var isMoving:boolean = false;
+var catchAudio:AudioClip;
+
+
+function Start () {
+	animation.Stop();	
+}
+
+function Update () {
+	var halfW:float = Screen.width / 2;
+	transform.position.x = (Input.mousePosition.x)/20;	
+	
+	if(lastX != transform.position.x){
+		//if(!isMoving){
+		//Debug.Log('moving');
+			isMoving = true;
+			if(!animation.IsPlaying("catch")){			
+				 animation.CrossFade("step");	
+				Debug.Log("step");						
+			}						
+		//}
+	}
+	else{
+		//if(isMoving){
+		//Debug.Log('stop');
+			isMoving = false;
+			if(!animation.IsPlaying("catch")){			
+				Debug.Log("idle");
+				animation.CrossFade("idle");
+			}					
+				
+		//}		
+	}		
+	lastX = transform.position.x;			
+}
+
+function OnCollisionEnter(col : Collision) {
+	if(col.gameObject.CompareTag("bomb")){		
+		
+	} else if(col.gameObject.CompareTag("stein")){		
+		audio.PlayOneShot(catchAudio);
+		Debug.Log('catch');
+		animation.Play("catch");
+	}
+	
+	col.gameObject.transform.position.y = 50;
+	col.gameObject.transform.position.z = -16;
+	col.gameObject.transform.position.x = Random.Range(0,60);
+	
+}
---
+++ Assets/Scripts/Falling Object.js
@@ -0,0 +1,20 @@
+﻿#pragma strict
+
+var prefab:GameObject;
+var speed:int;
+var audioClips:AudioClip[];
+
+function Start () {
+
+}
+
+function Update () {
+	transform.position.y -= speed * Time.deltaTime;	
+	if(transform.position.y < 0){
+		audio.PlayOneShot(audioClips[Random.Range(0,audioClips.length)]);
+		Instantiate(prefab, transform.position,Quaternion.identity);
+		transform.position.y = 50;
+		transform.position.x = Random.Range(0,60);
+		transform.position.z = -16;
+		}	
+}
